cddd294536d4b379feb2454c4144156042f67b5a
Hoang Nghia <hoangnghianguyendac@gmail.com>
2022-10-05 08:06:03 +0700

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello this is Hoang Nghia Nguyen Dac
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+import Menu from "./components/MenuComponent";
+import { Navbar, NavbarBrand } from "reactstrap";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+      
+    );
+  }
 }
 
 export default App;
