50368dfe519fd84af5e8d9f4cdff7e5df418a7d8
Hoang Nghia <hoangnghianguyendac@gmail.com>
2022-11-01 16:00:27 +0700

    redux thunk and redux form revisited

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -10,6 +10,7 @@ import {
   BreadcrumbItem,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import Loading from "./LoadingComponent";
 function RenderDish(dish) {
   if (dish != null) {
     return (
@@ -58,8 +59,23 @@ function RenderComments(comments, dishId, addComment) {
 
 const DishDetail = (props) => {
   console.log("Check props dishdetail>>>", props);
-
-  if (props.dish != null) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
@@ -78,7 +94,6 @@ const DishDetail = (props) => {
           <div className="col-12 col-md-5 m-1">{RenderDish(props.dish)}</div>
           <div className="col-12 col-md-5 m-1">
             {RenderComments(props.comments, props.dish.id, props.addComment)}
-           
           </div>
         </div>
       </div>
