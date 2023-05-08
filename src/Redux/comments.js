2373b00c45afb44563e12ab970b4447b1371d999
HoangNghia <hoangnghianguyendac@gmail.com>
2023-05-08 10:01:06 +0700

    QQQQ

--- src/Redux/comments.js
+++ src/Redux/comments.js
@@ -7,8 +7,6 @@ export const Comments=(state={
     switch(action.type){
         case ActionTypes.ADD_COMMENT:
             var comment=action.payload;
-            comment.id=state.length;
-            comment.date=new Date().toISOString();
             return {...state,comments:state.comments.concat(comment)};
         case ActionTypes.ADD_COMMENTS:
             return {...state,errMess:null,comments:action.payload};
